fb9d0aea308dc5266fd8b65fc48ba7df4f17efb5
Susisu <susisu2413@yahoo.co.jp>
2016-10-13 22:36:42 +0900

    Add 1K JSON

--- parsing/loquat_naive.js
+++ parsing/loquat_naive.js
@@ -80,18 +80,19 @@ function parse(src) {
     }
 }
 
-const fs = require("fs");
-const util = require("util");
-fs.readFile("./sample.json", "utf8", (err, src) => {
-    if (err) {
-        console.error(err);
-        process.exit(1);
-    }
+// const fs = require("fs");
+// const util = require("util");
+// fs.readFile("./sample.json", "utf8", (err, src) => {
+//     if (err) {
+//         console.error(err);
+//         process.exit(1);
+//     }
+    const src1k = require("./1K_json.js");
     let data;
     console.time("parse");
-    for (let k = 0; k < 100; k++) {
-        data = parse(src);
+    for (let k = 0; k < 1000; k++) {
+        data = parse(src1k);
     }
     console.timeEnd("parse");
-    console.log(util.inspect(data, { depth: null, colors: true }));
-});
+//     console.log(util.inspect(data, { depth: null, colors: true }));
+// });
--- parsing/peg.js
+++ parsing/peg.js
@@ -2,18 +2,19 @@
 
 const parser = require("./peg_parser.js");
 
-const fs = require("fs");
-const util = require("util");
-fs.readFile("./sample.json", "utf8", (err, src) => {
-    if (err) {
-        console.error(err);
-        process.exit(1);
-    }
+// const fs = require("fs");
+// const util = require("util");
+// fs.readFile("./sample.json", "utf8", (err, src) => {
+//     if (err) {
+//         console.error(err);
+//         process.exit(1);
+//     }
+    const src1k = require("./1K_json.js");
     let data;
     console.time("parse");
-    for (let k = 0; k < 100; k++) {
-        data = parser.parse(src);
+    for (let k = 0; k < 1000; k++) {
+        data = parser.parse(src1k);
     }
     console.timeEnd("parse");
-    console.log(util.inspect(data, { depth: null, colors: true }));
-});
+//     console.log(util.inspect(data, { depth: null, colors: true }));
+// });
