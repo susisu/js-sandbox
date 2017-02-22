cc2ca66d2f5097edb428cef648feaa14495ec1e5
Susisu <susisu2413@yahoo.co.jp>
2017-02-22 14:00:39 +0900

    Add duration property instead of start/endTime

--- tween/lib/tween.js
+++ tween/lib/tween.js
@@ -1,16 +1,11 @@
 "use strict";
 
 class Tween {
-  constructor(startTime, endTime, initialize, animate, finalize, easing) {
-      this.startTime  = startTime;
-      this.endTime    = endTime;
-      this.initialize = initialize;
-      this.animate    = animate;
-      this.finalize   = finalize;
-      this.ease       = ease;
-  }
-
-  get duration() {
-      return this.endTime - this.startTime;
-  }
+    constructor(duration, initialize, animate, finalize, easing) {
+        this.duration   = duration;
+        this.initialize = initialize;
+        this.animate    = animate;
+        this.finalize   = finalize;
+        this.ease       = ease;
+    }
 }
