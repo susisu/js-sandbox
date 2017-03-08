a34a923405e4a34b29043c806939d3d748a845e3
Susisu <susisu2413@yahoo.co.jp>
2017-03-08 18:10:37 +0900

    Add `TweenInstance` and `Timeline` classes

--- tween/lib/tween.js
+++ tween/lib/tween.js
@@ -9,3 +9,23 @@ class Tween {
         this.ease       = ease;
     }
 }
+
+class TweenInstance {
+    constructor(startTime, tween) {
+        this.startTime = startTime;
+        this.tween     = tween;
+        this.context   = {};
+    }
+}
+
+class Timeline {
+    constructor() {
+        this._currentTime = 0;
+        this._pendingInstances = new Map();
+        this._activeInstances  = new Map();
+    }
+
+    get currentTime() {
+        return this._currentTime;
+    }
+}
