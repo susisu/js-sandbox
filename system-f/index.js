df9239348b36a79bad932ac0cc6e13897a40e51a
Susisu <susisu2413@yahoo.co.jp>
2017-05-27 12:25:02 +0900

    Implement REPL-like CLI

--- system-f/index.js
+++ system-f/index.js
@@ -1,36 +1,61 @@
 // @flow
 /* eslint-disable no-console */
 
+import { createInterface } from "readline";
+
 import { parse } from "./lib/parser.js";
 import { emptyContext } from "./lib/term.js";
 
-const src = `
-Variable False;
-Axiom ex: forall X. False -> X;
-Axiom dne: forall X. ((X -> False) -> False) -> X;
-Theorem peirce: forall A, B. ((A -> B) -> A) -> A
-= fun2 A.
-  fun2 B.
-  dne [((A -> B) -> A) -> A] (
-    fun H: (((A -> B) -> A) -> A) -> False.
-    H (
-      fun H0: (A -> B) -> A.
-      H0 (
-        fun H1: A.
-        ex [B] (
-          H (
-            fun H2: (A -> B) -> A.
-            H1
-          )
-        )
-      )
-    )
-  );
-`;
-
-const stmts = parse(src);
 let context = emptyContext();
-for (let i = 0; i < stmts.length; i++) {
-  const st = stmts[i];
-  context = st.exec(context);
+
+function interact() {
+  const reader = createInterface({
+    input : process.stdin,
+    output: process.stdout
+  });
+
+  let buffer = "";
+  reader.setPrompt("> ");
+  reader.prompt();
+
+  reader.on("line", line => {
+    buffer += line + "\n";
+    const semi = buffer.indexOf(";");
+    if (semi >= 0) {
+      let stmts;
+      try {
+        stmts = parse(buffer);
+        for (let i = 0; i < stmts.length; i++) {
+          const stmt = stmts[i];
+          context = stmt.exec(context);
+        }
+      }
+      catch (err) {
+        if (err instanceof Error) {
+          process.stdout.write(err.message + "\n");
+        }
+        else {
+          process.stdout.write(String(err));
+        }
+      }
+      buffer = "";
+      reader.setPrompt("> ");
+      reader.prompt();
+    }
+    else {
+      reader.setPrompt("| ");
+      reader.prompt();
+    }
+  });
+
+  reader.on("close", () => {
+    interact();
+  });
+
+  reader.on("SIGINT", () => {
+    process.stdout.write("\n");
+    process.exit(0);
+  });
 }
+
+interact();
