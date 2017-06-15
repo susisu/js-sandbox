ec77210d091170c6c38fb227ca85c2c4c15c2c03
Susisu <susisu2413@yahoo.co.jp>
2017-06-16 01:59:12 +0900

    Add indexed term

---
+++ coc/lib/ixterm.js
@@ -0,0 +1,167 @@
+// @flow
+
+import type { Showable } from "./common.js";
+
+export class Term {
+  pos: Showable;
+
+  constructor(pos: Showable) {
+    this.pos = pos;
+  }
+
+  toString(): string {
+    throw new Error("not implemented");
+  }
+
+  shift(cut: number, dist: number): Term {
+    throw new Error("not implemented");
+  }
+
+  subst(index: number, term: Term): Term {
+    throw new Error("not implemented");
+  }
+}
+
+export class TmVar extends Term {
+  index: number;
+
+  constructor(pos: Showable, index: number) {
+    super(pos);
+    this.index = index;
+  }
+
+  toString(): string {
+    return this.index.toString();
+  }
+
+  shift(cut: number, dist: number): Term {
+    return this.index >= cut
+      ? new TmVar(this.pos, this.index + dist)
+      : this;
+  }
+
+  subst(index: number, term: Term): Term {
+    return this.index === index
+      ? term
+      : this;
+  }
+}
+
+export class TmAbs extends Term {
+  paramType: Term;
+  body: Term;
+
+  constructor(pos: Showable, paramType: Term, body: Term) {
+    super(pos);
+    this.paramType = paramType;
+    this.body      = body;
+  }
+
+  toString(): string {
+    return "fun : " + this.paramType.toString()
+      + ". " + this.body.toString();
+  }
+
+  shift(cut: number, dist: number): Term {
+    return new TmAbs(
+      this.pos,
+      this.paramType.shift(cut, dist),
+      this.body.shift(cut + 1, dist)
+    );
+  }
+
+  subst(index: number, term: Term): Term {
+    return new TmAbs(
+      this.pos,
+      this.paramType.subst(index, term),
+      this.body.subst(index + 1, term.shift(0, 1))
+    );
+  }
+}
+
+export class TmApp extends Term {
+  func: Term;
+  arg: Term;
+
+  constructor(pos: Showable, func: Term, arg: Term) {
+    super(pos);
+    this.func = func;
+    this.arg  = arg;
+  }
+
+  toString(): string {
+    const funcStr = this.func instanceof TmVar || this.func instanceof TmApp
+      ? this.func.toString()
+      : "(" + this.func.toString() + ")";
+    const argStr = this.arg instanceof TmVar
+      ? this.arg.toString()
+      : "(" + this.arg.toString() + ")";
+    return funcStr + " " + argStr;
+  }
+
+  shift(cut: number, dist: number): Term {
+    return new TmApp(
+      this.pos,
+      this.func.shift(cut, dist),
+      this.arg.shift(cut, dist)
+    );
+  }
+
+  subst(index: number, term: Term): Term {
+    return new TmApp(
+      this.pos,
+      this.func.subst(index, term),
+      this.arg.subst(index, term)
+    );
+  }
+}
+
+export class TmProd extends Term {
+  paramType: Term;
+  body: Term;
+
+  constructor(pos: Showable, paramType: Term, body: Term) {
+    super(pos);
+    this.paramType = paramType;
+    this.body      = body;
+  }
+
+  toString(): string {
+    return "( : " + this.paramType.toString()
+      + ") -> " + this.body.toString();
+  }
+
+  shift(cut: number, dist: number): Term {
+    return new TmProd(
+      this.pos,
+      this.paramType.shift(cut, dist),
+      this.body.shift(cut + 1, dist)
+    );
+  }
+
+  subst(index: number, term: Term): Term {
+    return new TmProd(
+      this.pos,
+      this.paramType.subst(index, term),
+      this.body.subst(index + 1, term.shift(0, 1))
+    );
+  }
+}
+
+export class TmProp extends Term {
+  constructor(pos: Showable) {
+    super(pos);
+  }
+
+  toString(): string {
+    return "*";
+  }
+
+  shift(cut: number, dist: number): Term {
+    return this;
+  }
+
+  subst(index: number, term: Term): Term {
+    return this;
+  }
+}
