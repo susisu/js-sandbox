a601428b7927eebc3be3d25c03ea5fbc332fc3af
Susisu <susisu2413@yahoo.co.jp>
2017-06-17 15:53:40 +0900

    Add utility functions

--- coc/lib/common.js
+++ coc/lib/common.js
@@ -3,3 +3,28 @@
 export type Showable = { toString: () => string } | string;
 
 export const INTERNAL_POS = "<internal>";
+
+export function generateVarName(id: number): string {
+  let name = "";
+  let n    = id;
+  while (n >= 0) {
+    name = String.fromCharCode((n % 26) + "a".charCodeAt(0)) + name;
+    n    = (n / 26 >> 0) - 1;
+  }
+  return name;
+}
+
+export function createReferenceError(pos: Showable, message: string): Error {
+  return new Error(
+      `Reference Error at ${pos.toString()}:\n`
+    + `  ${message}`
+  );
+}
+
+export function createTypeError(pos: Showable, expected: string, actual: string): Error {
+  return new Error(
+      `Type Error at ${pos.toString()}:\n`
+    + `  expected: ${expected}\n`
+    + `  actual  : ${actual}`
+  );
+}
