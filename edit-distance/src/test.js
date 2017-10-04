d88e926747e9eb2a31f7449779eb4f0cf3555354
Susisu <susisu2413@yahoo.co.jp>
2017-10-05 00:25:51 +0900

    Add two test cases

--- edit-distance/src/test.js
+++ edit-distance/src/test.js
@@ -9,4 +9,6 @@ module.exports.test = distanceFunc => {
   assert(distanceFunc("", "ABC") === 3);
   assert(distanceFunc("foobarbaz", "fooxbrbiz") === 4);
   assert(distanceFunc("fooxbrbiz", "foobarbaz") === 4);
+  assert(distanceFunc("kitten", "sitting") === 5);
+  assert(distanceFunc("sitting", "kitten") === 5);
 };
